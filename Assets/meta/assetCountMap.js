8447ed24171345ec2e31a867d1fdbb4641a59e98
Morabaraba May <morabaraba@openhazel.co.za>
2017-02-25 20:38:44 +0200

    adding more assets

---
+++ Assets/meta/assetCountMap.js
@@ -0,0 +1,4 @@
+assetCountMap = {
+  "Temp/scene_editor.bin": 14,
+  "Assets/scene/tweeter-trouble.bin": 14
+};
---
+++ Assets/meta/globalUrlMap.js
@@ -0,0 +1,18 @@
+urlMapConfig = {
+  "758b1a09-36ca-49c1-b92f-c2a4db708e71": "Assets/audio/Mr. Oizo - Tweeter Trouble.mp3.bin",
+  "ede82f60-64c4-47bd-91e0-3834eac42286": "Assets/scene/tweeter-trouble.bin",
+  "307c180d-9389-4ea4-81f3-24712c686f08": "Assets/sprite/backdrop.bin",
+  "f8b6383b-7c2f-4e08-bb31-e175a5268413": "Assets/sprite/bassbody.bin",
+  "a05d01ee-fc00-4994-95ec-a54d136a0a58": "Assets/sprite/basshead.bin",
+  "9251ed48-cd25-4179-818f-0683a4111971": "Assets/sprite/basshead_turned.bin",
+  "ff55e221-cc5b-438a-9727-a59f20f10467": "Assets/sprite/bigbird.bin",
+  "ccfe8e80-30ff-431d-93ca-0b6de7004666": "Assets/sprite/bigbird_finger.bin",
+  "c478220d-f887-4175-8019-2a8c5c3892e2": "Assets/sprite/handy.bin",
+  "a8477037-27b0-4c29-93e8-f1db51220539": "Assets/sprite/mroizo.bin",
+  "79cf19b8-3b38-43cd-a545-1753ce100d16": "Assets/sprite/orangeball.bin",
+  "dd8c0751-122f-40c1-814e-f7fb9f53812f": "Assets/sprite/piano.bin",
+  "bf8172ac-0a84-453b-b3a1-faef4fdd2e54": "Assets/sprite/skater.bin",
+  "637c667e-ce64-426d-910f-d0fd3f876efa": "Assets/sprite/table.bin",
+  "062157ea-b6f8-4f03-af05-66577ce19b68": "Assets/sprite/yellowball.bin",
+  "76598e8b-1025-4f52-b0f6-288e40b9310c": "Temp/scene_editor.bin"
+};
